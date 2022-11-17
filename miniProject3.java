e8a304eef0c134f8bc4e82895ff7d8011dc00a3e
devraj894 <118315477+devraj894@users.noreply.github.com>
2022-11-17 06:39:18 +0530

    Update miniProject3.java

--- miniProject3.java
+++ miniProject3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class miniProject2{
+public class miniProject3{
     public static void main(String[] args){
         
         Scanner sc=new Scanner(System.in);
